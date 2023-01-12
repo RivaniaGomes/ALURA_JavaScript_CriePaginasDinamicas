487de6fd1882d648bd0a357648d16fe7b403ae01
RivaniaGomes <rivaniagomes@gmail.com>
2023-01-12 18:19:43 -0300

    Eventos e lógicas

--- js/desafio1.js
+++ js/desafio1.js
@@ -9,3 +9,18 @@ for (indice = 0; indice < listaDeTeclas.length; indice++) {
     inputTel.value = inputTel.value + tecla.value;
   }
 }
+
+// const listaDeTeclas = document.querySelectorAll('input[type=button]');
+
+// for (let indice = 0; indice < listaDeTeclas.length; indice++) {
+//   const tecla = listaDeTeclas[indice];
+
+//   tecla.onkeydown = function (evento) {
+//     if(evento.code === "Enter" || evento.code === "Space") {
+//     tecla.classList.add('ativa');
+//     }
+//   }
+//   tecla.onkeyup = function () {
+//     tecla.classList.remove('ativa');
+//   }
+// }
